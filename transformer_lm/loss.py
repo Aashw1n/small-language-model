eee9c692c7358ebad6b3efbd52de26cfa7fe9a41
Aashw1n <aashwinranjan98@gmail.com>
2026-07-30 20:32:20 -0700

    memory optimization for loss

--- transformer_lm/loss.py
+++ transformer_lm/loss.py
@@ -1,11 +1,6 @@
+import torch
 
 def cross_entropy_loss(logits, targets):
-    # Get max logits
-    m = logits.max(dim=-1, keepdim=True).values
-    shifted = logits - m
-    lse = shifted.exp().sum(dim=-1).log()
+    lse = torch.logsumexp(logits, dim=-1)
     target_logits = logits.gather(dim=-1, index=targets.unsqueeze(-1)).squeeze(-1)
-    loss_per_position = m.squeeze(-1) + lse - target_logits
-
-
-    return loss_per_position.mean()
+    return (lse - target_logits).mean()
